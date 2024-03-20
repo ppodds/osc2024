c186d37ecf9dee9f62884e5c250a9dbdd33a9917
ppodds <oscar20020629@gmail.com>
2024-03-20 14:06:40 +0000

    feat: transfer kernel with progress hint

--- bins/kernel-transfer/src/main.rs
+++ bins/kernel-transfer/src/main.rs
@@ -1,4 +1,6 @@
 use std::io::prelude::Write;
+use std::thread::sleep;
+use std::time::Duration;
 use std::{env::args, fs::File, io::Read};
 
 const HELP_MESSAGE: &str = "Usage: kernel-transfer <tty> <kernel image>";
@@ -20,8 +22,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut tty = File::create(tty_path)?;
     println!("Sending protocal header...");
     tty.write_all(&size.to_le_bytes())?;
+    let mut current = 0;
+    // write 1024 bytes as a block to prevent data loss
+    const BLOCK_SIZE: usize = 1024;
     println!("Sending kernel image...");
-    tty.write_all(buf.as_slice())?;
+    while current + BLOCK_SIZE < buf.len() {
+        tty.write_all(&buf[current..current + BLOCK_SIZE])?;
+        current += BLOCK_SIZE;
+        println!(
+            "Progress: {}/{} ({:.1}%)",
+            current,
+            buf.len(),
+            (current as f32 / buf.len() as f32) * 100.0
+        );
+        // prevent data loss
+        sleep(Duration::from_millis(100));
+    }
+    tty.write_all(&buf[current..buf.len()])?;
     println!("Send complete.");
     Ok(())
 }
