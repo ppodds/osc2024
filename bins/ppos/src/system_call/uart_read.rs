390a210bce64b85ad8ecf278f255c4906d0c809c
ppodds <oscar20020629@gmail.com>
2024-05-02 18:55:25 +0000

    rollback: fast return cause syscall.img execute failed

--- bins/ppos/src/system_call/uart_read.rs
+++ bins/ppos/src/system_call/uart_read.rs
@@ -4,13 +4,13 @@ use library::console::console;
 
 pub fn uart_read(buf: *mut u8, size: usize) -> usize {
     let mut buffer = unsafe { slice::from_raw_parts_mut(buf, size) };
-    let mut i = 0;
-    while let Some(c) = console().read_char() {
-        buffer[i] = c as u8;
-        i += 1;
-        if i == size {
-            break;
+    for i in 0..size {
+        loop {
+            if let Some(c) = console().read_char() {
+                buffer[i] = c as u8;
+                break;
+            }
         }
     }
-    i
+    size
 }
