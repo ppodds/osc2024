43098c75da2eb0a51517108094d7d8530dfe2031
ppodds <oscar20020629@gmail.com>
2026-03-03 18:06:51 +0800

    fix: read interrupt of mini uart is never triggered

--- bins/ppos/src/device/mini_uart.rs
+++ bins/ppos/src/device/mini_uart.rs
@@ -42,6 +42,8 @@ struct MiniUartInner {
     read_buffer: VecDeque<u8>,
     write_buffer: VecDeque<u8>,
     mode: ConsoleMode,
+    receiver_has_valid_byte: bool,
+    writer_is_empty: bool,
 }
 
 pub struct MiniUart {
@@ -169,6 +171,8 @@ impl MiniUartInner {
             read_buffer: VecDeque::new(),
             write_buffer: VecDeque::new(),
             mode: ConsoleMode::Sync,
+            receiver_has_valid_byte: false,
+            writer_is_empty: false,
         }
     }
 
@@ -282,32 +286,26 @@ impl MiniUartInner {
     }
 
     fn handle_interrupt(&mut self) {
-        match self
-            .registers
-            .interrupt_identify
-            .read_as_enum(AUX_MU_IIR::INTERRUPT_ID_BITS)
-        {
-            Some(AUX_MU_IIR::INTERRUPT_ID_BITS::Value::NO_INTERRUPT) => (),
-            Some(AUX_MU_IIR::INTERRUPT_ID_BITS::Value::TRANSMIT_HOLDING_REGISTER_EMPTY) => {
-                // if nothing to write, disable write interrupt
-                // or it will keep firing and racing cpu
-                self.disable_write_interrupt();
-                if let Some(byte) = self.write_buffer.pop_front() {
-                    self.write_byte(byte);
-                    self.enable_write_interrupt();
-                }
+        if self.receiver_has_valid_byte {
+            let byte = self.read_byte();
+            self.read_buffer.push_back(byte);
+            self.receiver_has_valid_byte = false;
+            self.enable_read_interrupt();
+            return;
+        } else if self.writer_is_empty {
+            // if nothing to write, disable write interrupt
+            // or it will keep firing and racing cpu
+            self.disable_write_interrupt();
+            if let Some(byte) = self.write_buffer.pop_front() {
+                self.write_byte(byte);
+                self.writer_is_empty = false;
+                self.enable_write_interrupt();
             }
-            Some(AUX_MU_IIR::INTERRUPT_ID_BITS::Value::RECEIVER_HOLDS_VAILD_BYTE) => {
-                self.disable_read_interrupt();
-                let byte = self.read_byte();
-                self.read_buffer.push_back(byte);
-                self.enable_read_interrupt();
-            }
-            None => panic!("Invalid interrupt"),
         }
     }
 
     fn interrupt_prehook(&mut self) {
+        
         match self
             .registers
             .interrupt_identify
@@ -316,9 +314,11 @@ impl MiniUartInner {
             Some(AUX_MU_IIR::INTERRUPT_ID_BITS::Value::NO_INTERRUPT) => (),
             Some(AUX_MU_IIR::INTERRUPT_ID_BITS::Value::TRANSMIT_HOLDING_REGISTER_EMPTY) => {
                 self.disable_write_interrupt();
+                self.writer_is_empty = true;
             }
             Some(AUX_MU_IIR::INTERRUPT_ID_BITS::Value::RECEIVER_HOLDS_VAILD_BYTE) => {
                 self.disable_read_interrupt();
+                self.receiver_has_valid_byte = true;
             }
             None => panic!("Invalid interrupt"),
         }
